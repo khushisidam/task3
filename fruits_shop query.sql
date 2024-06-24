501fb5fd91de11a2be1146bc752b58ea119479a8
khushisidam <khushi.sidam24@gmail.com>
2024-06-24 12:35:37 +0530

    task3 completed

---
+++ fruits_shop query.sql
@@ -0,0 +1,26 @@
+select * from  fruits_shop
+
+select fruits_name from fruits_shop
+
+select category from fruits_shop
+
+select * from  fruits_shop
+
+select category, price from fruits_shop
+
+select stock_quantity, supplier from fruits_shop
+
+select price,stock_quantity,supplier  from fruits_shop
+
+select * from fruits_shop where fruits_name = 'apple'
+
+select * from fruits_shop where stock_quantity > 50
+
+select * from fruits_shop where price < 70
+
+select * from fruits_shop where fruits_name = 'mango'
+
+
+
+
+
